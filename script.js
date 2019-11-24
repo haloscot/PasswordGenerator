42663d8af7036531810e565c718feda843029670
haloscot <hannahlorrainescott@gmail.com>
2019-11-23 19:25:56 -0500

    updated css and html for styling

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-
 function generate(){
-  let length = document.getElementById("pwLength").value;
-  let characters = "ASDFGHJKLQWERTYUIOPZXCVBNMasdfghjklqwertyuiopzxcvbnm1234567890!@#$%^&*";
-  let password = ""
+
+var length = document.getElementById("pwLength").value;
+var characters = "ASDFGHJKLQWERTYUIOPZXCVBNMasdfghjklqwertyuiopzxcvbnm1234567890!@#$%^&*";
+var password = ""
 
   for(var i = 0; i <= length; i++){
     password = password + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length - 1)));
