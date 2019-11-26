4f01de75940dcd6dd6a111e9ae641b75071d2191
haloscot <hannahlorrainescott@gmail.com>
2019-11-26 18:54:34 -0500

    updated css and html for styling, added documenting previous passwords

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ var password = ""
 
   for(var i = 0; i <= length; i++){
     password = password + characters.charAt(Math.floor(Math.random() * Math.floor(characters.length - 1)));
-
-    document.getElementById("display").value = password;
   }
 
+  document.getElementById("display").value = password;
+
+  document.getElementById("prevPW").innerHTML += password + "<br />";
 }
